40b95fce1082b73bfde76debb7d523adeffb6952
Alex Armengou <alex.armengou@gmail.com>
2018-04-05 13:44:05 +0200

    Create activities

---
+++ frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/MailLoginActivity.java
@@ -0,0 +1,16 @@
+package com.becitizen.app.becitizen.presentation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.becitizen.app.becitizen.R;
+
+
+public class MailLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
