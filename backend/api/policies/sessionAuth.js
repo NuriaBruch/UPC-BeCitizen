43b7531d8e5778fd8a4cf4176c0faa3a88c12daa
Omar Garcia <omarcruz11@hotmai.com>
2018-04-06 13:10:17 +0200

    User with sessions

--- backend/api/policies/sessionAuth.js
+++ backend/api/policies/sessionAuth.js
@@ -17,5 +17,9 @@ module.exports = function(req, res, next) {
 
   // User is not allowed
   // (default res.forbidden() behavior can be overridden in `config/403.js`)
-  return res.forbidden('You are not permitted to perform this action.');
+  //return res.forbidden('You are not permitted to perform this action.');
+  return res.send({
+    status: "Error",
+    errors: ["User not logged in"]
+  });
 };
