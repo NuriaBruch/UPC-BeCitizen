3c1bebb36c11037aee87bcc89fa3bc19b3c2ed73
NuriaBruch <36881918+NuriaBruch@users.noreply.github.com>
2018-04-06 12:16:27 +0200

    Modificacions a la vista registre mail

--- frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/ViewSignUpMail.java
+++ frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/ViewSignUpMail.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
@@ -37,16 +38,15 @@ public class ViewSignUpMail extends AppCompatActivity /*implements View.OnClickL
 
         tietMail = findViewById(R.id.tietMail);
         tietPassw = findViewById(R.id.tietPassw);
+        tietPassw.setTransformationMethod(new PasswordTransformationMethod());
         tietPassw2 = findViewById(R.id.tietPassw2);
+        tietPassw2.setTransformationMethod(new PasswordTransformationMethod());
     }
 
     public void signUp(View view) {
         if (!validateEmail()) return;
         if (!validatePassw()) return;
         if (!validatePassw2()) return;
-
-        Toast notificacion = Toast.makeText(this,"You subbmitted your data" ,Toast.LENGTH_SHORT);
-        notificacion.show();
 
         Intent i=new Intent(this,DataRegisterView.class);
         i.putExtra("email", tietMail.getText().toString());
@@ -86,8 +86,14 @@ public class ViewSignUpMail extends AppCompatActivity /*implements View.OnClickL
         String pass2 = tietPassw2.getText().toString();
         boolean b = true;
 
-        if (pass2.trim().isEmpty() || !pass2.equals(pass)) {
+        if (pass2.trim().isEmpty()) {
             tietPassw2.setError(getString(R.string.errorMsgName));
+            requestFocus(tietPassw2);
+            b = false;
+        }
+
+        else if (b && !pass2.equals(pass)) {
+            tietPassw2.setError(getString(R.string.errorPassw));
             requestFocus(tietPassw2);
             b = false;
         }
