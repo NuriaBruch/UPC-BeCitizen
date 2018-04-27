c75985ff722762b7c22f3b03257a6a2670b2da36
Guillem <guillem.bonet@gmail.com>
2018-04-27 13:23:59 +0200

    CategoryThread progress

--- frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/CategoryThreadAdapter.java
+++ frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/CategoryThreadAdapter.java
@@ -11,7 +11,10 @@ import android.widget.TextView;
 import com.becitizen.app.becitizen.R;
 import com.becitizen.app.becitizen.domain.entities.CategoryThread;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class CategoryThreadAdapter extends ArrayAdapter<CategoryThread> implements View.OnClickListener{
 
@@ -61,11 +64,45 @@ public class CategoryThreadAdapter extends ArrayAdapter<CategoryThread> implemen
 
         // 4. Set the text for textView
         titleView.setText(dataSet.get(position).getTitle());
-        subtitleView.setText(dataSet.get(position).getCreatedAt().toString() + subtitleView.getText() +
+
+        //get x seconds/hours/days/years string
+        String dateString;
+        try{
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            cal.setTime(sdf.parse(dataSet.get(position).getCreatedAt()));
+
+            Calendar calendar = Calendar.getInstance();
+            long now = calendar.getTimeInMillis();
+            long time = cal.getTimeInMillis();
+
+            long diff = now - time;
+
+            int seconds = (int) (diff / 1000);
+            int minutes = (int) (diff / (1000*60));
+            int hours   = (int) (diff / (1000*60*60));
+            int days = (int) (diff / (1000*60*60*24));
+
+            if (seconds == 1) dateString = String.valueOf(seconds) + " " + context.getResources().getString(R.string.second);
+            else if (seconds < 60) dateString = String.valueOf(seconds) + " " + context.getResources().getString(R.string.seconds);
+            else if (minutes == 1) dateString = String.valueOf(minutes) + " " + context.getResources().getString(R.string.minute);
+            else if (minutes < 60) dateString = String.valueOf(minutes) + " " + context.getResources().getString(R.string.minutes);
+            else if (hours == 1) dateString = String.valueOf(hours) + " " + context.getResources().getString(R.string.hour);
+            else if (hours < 24) dateString = String.valueOf(hours) + " " + context.getResources().getString(R.string.hours);
+            else if (days == 1) dateString = String.valueOf(days) + " " + context.getResources().getString(R.string.day);
+            else if (days < 365) dateString = String.valueOf(days) + " " + context.getResources().getString(R.string.days);
+            else if (days/365 == 1) dateString = String.valueOf(days/365) + " " + context.getResources().getString(R.string.year);
+            else dateString = String.valueOf(days/365) + " " + context.getResources().getString(R.string.years);
+
+        }catch(ParseException e){
+            dateString = "";
+            System.out.println(e.toString());
+        }
+
+        subtitleView.setText(dateString + " " + subtitleView.getText() + " " +
             dataSet.get(position).getAuthor());
         votesView.setText(String.valueOf(dataSet.get(position).getVotes()));
 
-        // 5. retrn rowView
         return rowView;
     }
 }
