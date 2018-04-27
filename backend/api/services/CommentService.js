375a81a0d33feb083e1074f9976b9791cfa4ed23
Omar Garcia <omarcruz11@hotmail.com>
2018-04-27 21:33:14 +0200

    Report comments done

--- backend/api/services/CommentService.js
+++ backend/api/services/CommentService.js
@@ -9,24 +9,22 @@ module.exports = {
         User.findOne({email: email}).populate("reportedComments").exec(function (err, user){
             if((err && err !== undefined) || user == undefined) {
                 response.status = "E1";
-                response.errors.push("No user");
+                response.errors.push("There is no user with that id.");
                 callback(response);
             }
             else{
                 Comment.findOne({id: commentId}).populate("reportedBy").exec(function(err1, comment){
                     if((err1 && err1 !== undefined) || comment == undefined) {
                         response.status = "E2";
-                        response.errors.push("No comment with that id");
+                        response.errors.push("There is no comment with that id.");
                         callback(response);
                     }
                     else{
-                        megaUser = _.chain(user.toJSON());
-                        var found = megaUser.pluck('reportedComments').indexOf(commentId).value();
-                        console.log(found);
-                        //found = 0;
+                        var found = _.chain(user.reportedComments).pluck("id").indexOf(commentId).value();
                         if(found === -1){
-                            if(comment.reportedBy.lenght > 0){ // <--
-                                comment.content = "This comment has been removed";
+                            const MAX_REPORTS = 10;
+                            if(_.size(comment.reportedBy) + 1 >= MAX_REPORTS){
+                                comment.content = "This comment has been deleted.";
                             }
                             user.reportedComments.add(commentId);
                             user.save(function(err){
@@ -38,7 +36,7 @@ module.exports = {
                         }
                         else{
                             response.status = "E3";
-                            response.errors.push("The user has reported that comment");
+                            response.errors.push("The user has already reported that comment.");
                             callback(response);
                         }
                     
