e937cabf2bbaf62c46ea562dc39aa96dec4919fe
Guillem <guillem.bonet@gmail.com>
2018-05-18 13:42:06 +0200

    InformationCategory Activity

--- frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/CategoryInformationAdapter.java
+++ frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/CategoryInformationAdapter.java
@@ -1,24 +1,22 @@
 package com.becitizen.app.becitizen.presentation;
 
-
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.becitizen.app.becitizen.R;
 import com.becitizen.app.becitizen.domain.entities.CategoryThread;
 import com.becitizen.app.becitizen.domain.entities.Information;
+import com.uncopt.android.widget.text.justify.JustifiedTextView;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 public class CategoryInformationAdapter extends RecyclerView.Adapter<CategoryInformationAdapter.ViewHolder> {
@@ -26,17 +24,21 @@ public class CategoryInformationAdapter extends RecyclerView.Adapter<CategoryInf
     private List<Information> mData;
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
+    private Context mContext;
 
     // stores and recycles views as they are scrolled off screen
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView title, content;
+        TextView title;
+        JustifiedTextView content;
         ImageView expander;
+        Button openInBrowser;
 
         ViewHolder(View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.infoTitle);
             content = itemView.findViewById(R.id.infoContent);
             expander = itemView.findViewById(R.id.expander);
+            openInBrowser = itemView.findViewById(R.id.openInBrowser);
             itemView.setOnClickListener(this);
         }
 
@@ -44,26 +46,54 @@ public class CategoryInformationAdapter extends RecyclerView.Adapter<CategoryInf
         public void onClick(View view) {
             if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
 
-            Information info = ControllerInformationPresentation.getUniqueInstance().getInformation(mData.get(getAdapterPosition()).getId());
+            Information info = null;
+
+            if (content.getVisibility() == View.GONE)
+                info = ControllerInformationPresentation.getUniqueInstance().getInformation(mData.get(getAdapterPosition()).getId());
+
             if (info != null) {
                 content.setText(info.getContent());
+                openInBrowser.setOnClickListener(new OpenInBrowserOnClick(info.getUrl()));
             }
 
             if (content.getVisibility() == View.VISIBLE) {
                 expander.setRotation(0);
                 content.setVisibility(View.GONE);
+                openInBrowser.setVisibility(View.GONE);
             }
             else {
                 expander.setRotation(180);
                 content.setVisibility(View.VISIBLE);
+                String url = info.getUrl();
+                if (url.startsWith("http://") || url.startsWith("https://")) {
+                    openInBrowser.setVisibility(View.VISIBLE);
+                }
             }
         }
     }
+
+    public class OpenInBrowserOnClick implements View.OnClickListener
+    {
+
+        String url;
+        public OpenInBrowserOnClick(String url) {
+            this.url = url;
+        }
+
+        @Override
+        public void onClick(View v)
+        {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            mContext.startActivity(browserIntent);
+        }
+
+    };
 
     // data is passed into the constructor
     CategoryInformationAdapter(Context context, List<Information> data) {
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
+        this.mContext = context;
     }
 
     // inflates the row layout from xml when needed
@@ -78,13 +108,14 @@ public class CategoryInformationAdapter extends RecyclerView.Adapter<CategoryInf
     public void onBindViewHolder(ViewHolder holder, int position) {
 
         holder.content.setVisibility(View.GONE);
+        holder.openInBrowser.setVisibility(View.GONE);
 
         if (mData.get(position).getTitle().length() < 22)
             holder.title.setText(mData.get(position).getTitle());
         else
             holder.title.setText(mData.get(position).getTitle().substring(0, 22) + "...");
 
-        holder.content.setText(mData.get(position).getContent());
+        //holder.content.setText(mData.get(position).getContent());
     }
 
     // total number of rows
