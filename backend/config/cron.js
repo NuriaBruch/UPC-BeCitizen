89c7f703f51d7c6117e5b31a9e52140221a74ea5
Omar Garcia <omarcruz11@hotmail.com>
2018-05-20 17:31:15 +0200

    Cron config

--- backend/config/cron.js
+++ backend/config/cron.js
@@ -1,14 +1,8 @@
 module.exports.cron = {
-    exchangeActualization: {
-      schedule: '* * 0 * * *',
-      onTick: function () {
-        MonetaryExchangeService.getAllCurrencies();
-      }
-    },
-    exchangeActualization2: {
-        schedule: '* * 12 * * *',
-        onTick: function () {
-          MonetaryExchangeService.getAllCurrencies();
-        }
-      }
-  };
+  tariffsUpdate: {
+    schedule: '* * 0 * * *',
+    onTick: function () {
+      TariffService.updateTariffs();
+    }
+  }
+};
