b53fe860d5cac127e05dcef016ee7d6657d1757d
NuriaBruch <36881918+NuriaBruch@users.noreply.github.com>
2018-05-24 09:23:10 +0200

    HelpActivity arreglada

--- frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/HelpActivity.java
+++ frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/HelpActivity.java
@@ -1,42 +1,28 @@
 package com.becitizen.app.becitizen.presentation;
 
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.TextView;
-
 import com.becitizen.app.becitizen.R;
 
-public class HelpActivity extends Fragment {
+/**
+ * Created by Nuria on 18/05/2018.
+ */
+
+public class HelpActivity extends Fragment implements View.OnClickListener{
 
     private View rootView;
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    private CardView cvUser;
+    private CardView cvInformation;
+    private CardView cvFAQ;
+    private CardView cvForum;
+    private CardView cvMessaging;
+    private CardView cvUtilities;
 
     public HelpActivity() {}
 
@@ -44,86 +30,51 @@ public class HelpActivity extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.activity_help, container, false);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getActivity().getSupportFragmentManager());
+        cvUser = rootView.findViewById(R.id.cvUser);
+        cvInformation = rootView.findViewById(R.id.cvInformation);
+        cvFAQ = rootView.findViewById(R.id.cvFAQ);
+        cvForum = rootView.findViewById(R.id.cvForum);
+        cvMessaging = rootView.findViewById(R.id.cvMessaging);
+        cvUtilities = rootView.findViewById(R.id.cvUtilities);
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) rootView.findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
-
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+        cvUser.setOnClickListener(this);
+        cvInformation.setOnClickListener(this);
+        cvFAQ.setOnClickListener(this);
+        cvForum.setOnClickListener(this);
+        cvMessaging.setOnClickListener(this);
+        cvUtilities.setOnClickListener(this);
 
         return rootView;
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    @Override
+    public void onClick(View view) {
+        final Dialog dialog = new Dialog(rootView.getContext());
+        switch (view.getId()) {
+            case R.id.cvUser:
+                dialog.setContentView(R.layout.tab1_user);
+                break;
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
+            case R.id.cvInformation:
+                dialog.setContentView(R.layout.tab2_information);
+                break;
+
+            case R.id.cvFAQ:
+                dialog.setContentView(R.layout.tab3_faq);
+                break;
+
+            case R.id.cvForum:
+                dialog.setContentView(R.layout.tab4_forum);
+                break;
+
+            case R.id.cvMessaging:
+                dialog.setContentView(R.layout.tab5_messaging);
+                break;
+
+            case R.id.cvUtilities:
+                dialog.setContentView(R.layout.tab6_utilities);
+                break;
         }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch(position) {
-                case 0:
-                    Tab1Principal tab1 = new Tab1Principal();
-                    return tab1;
-                case 1:
-                    Tab7User tab7 = new Tab7User();
-                    return tab7;
-                case 2:
-                    Tab2Information tab2 = new Tab2Information();
-                    return tab2;
-                case 3:
-                    Tab3FAQ tab3 = new Tab3FAQ();
-                    return tab3;
-                case 4:
-                    Tab4Forum tab4 = new Tab4Forum();
-                    return tab4;
-                case 5:
-                    Tab5Messaging tab5 = new Tab5Messaging();
-                    return tab5;
-                case 6:
-                    Tab6Utilities tab6 = new Tab6Utilities();
-                    return tab6;
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            return 7;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch(position) {
-                case 0:
-                    return "Principal";
-                case 1:
-                    return "Users";
-                case 2:
-                    return "Information";
-                case 3:
-                    return "FAQ";
-                case 4:
-                    return "Forum";
-                case 5:
-                    return "Messaging";
-                case 6:
-                    return "Utilities";
-            }
-
-            return null;
-        }
+        dialog.show();
     }
 }
