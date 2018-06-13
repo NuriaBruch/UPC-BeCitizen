3a571d74b9cf9a1140542d967869956c86c77470
Alex Armengou <alex.armengou@gmail.com>
2018-06-13 08:50:07 +0200

    Change password: hide keyboard

--- frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/user/ChangePasswordActivity.java
+++ frontend/BeCitizeN/app/src/main/java/com/becitizen/app/becitizen/presentation/user/ChangePasswordActivity.java
@@ -1,12 +1,14 @@
 package com.becitizen.app.becitizen.presentation.user;
 
+import android.content.Context;
+import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -64,6 +66,7 @@ public class ChangePasswordActivity extends Fragment  implements View.OnClickLis
                         newPassword.getText().toString().trim()
                 );
                 Toast.makeText(getContext(), getResources().getString(R.string.passUpdated), Toast.LENGTH_LONG).show();
+                hideKeyboard();
                 getActivity().onBackPressed();
 
             } catch (ServerException e) {
@@ -76,6 +79,14 @@ public class ChangePasswordActivity extends Fragment  implements View.OnClickLis
                 Toast.makeText(getContext(), getResources().getString(R.string.JSONerror), Toast.LENGTH_LONG).show();
                 Log.e("JSONe", e.getMessage());
             }
+        }
+    }
+
+    private void hideKeyboard() {
+        View view = getActivity().getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
 }
